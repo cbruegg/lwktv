604476464b5d21ece520a973e56991d3b4f081d2
Christian Brüggemann <mail@cbruegg.com>
2024-10-27 17:32:01 +0100

    Perform processing in one step

--- worker/src/lyrics-processor.ts
+++ worker/src/lyrics-processor.ts
@@ -1,8 +1,8 @@
 import { LrcLibGetLyricsResponse } from './lrclib-api-types';
 import OpenAI from 'openai';
+import { GetLyricsResponse } from './api-types';
 
 // TODO Add optional translation
-// TODO Optimization: Perform processing in a single OpenAI request by giving it multiple instructions + examples.
 
 export interface ProcessLyricsOptions {
 	traditionalToSimplified?: boolean;
@@ -16,37 +16,51 @@ export async function processLyrics(lrcLibLyrics: LrcLibGetLyricsResponse, openA
 	const synced = lrcLibLyrics.syncedLyrics !== undefined && lrcLibLyrics.syncedLyrics !== null;
 	const lyrics = lrcLibLyrics.syncedLyrics ?? lrcLibLyrics.plainLyrics;
 
+	console.log({ lyrics });
+
 	const openai = new OpenAI({ apiKey: openAiApiToken });
 
-	let processedLyrics = lyrics;
-
+	let systemInstructions = 'You operate on lyrics. You have the following tasks:\n';
 	if (traditionalToSimplified) {
-		const completion = await openai.chat.completions.create({
-			model: "gpt-4o",
-			messages: [
-				{ role: "system", content: "Translate the user input from traditional Chinese to simplified Chinese. Do not add any comments. Retain timestamps if there are any." },
-				{
-					role: "user",
-					content: processedLyrics,
-				},
-			],
-		});
-		processedLyrics = completion.choices[0].message.content ?? processedLyrics;
+		systemInstructions += '- Translate the user input from traditional Chinese to simplified Chinese. Do not add any comments. Retain timestamps if there are any.\n';
 	}
-
 	if (addPinyin) {
-		const completion = await openai.chat.completions.create({
-			model: "gpt-4o",
-			messages: [
-				{ role: "system", content: "Append transcribed Pinyin to each line of the user input. Place the Pinyin transcription right after the Chinese characters, separating them with ' ~~~ '. Do not add any comments. Retain timestamps if there are any." },
-				{
-					role: "user",
-					content: processedLyrics,
-				},
-			],
-		});
-		processedLyrics = completion.choices[0].message.content ?? processedLyrics;
+		systemInstructions += '- Append transcribed Pinyin to each line of the user input. Place the Pinyin transcription right after the Chinese characters, separating them with \' ~~~ \'. Do not add any comments. Retain timestamps if there are any.\n';
 	}
+	systemInstructions += 'For example, given the following input:\n';
+	systemInstructions += '```\n';
+	systemInstructions += '[00:26.92] 明年我計劃去中國和台灣旅行，探索當地的歷史文化和美食。\n' +
+		'[00:33.98] 我很期待和家人一起度過一個難忘的假期。\n';
+	systemInstructions += '```\n';
+	systemInstructions += 'You should output:\n';
+	systemInstructions += '```\n';
+	if (traditionalToSimplified && addPinyin) {
+		systemInstructions += '[00:26.92] 明年我计划去中国和台湾旅行，探索当地的历史文化和美食。míngnián wǒ jìhuà qù zhōngguó hé táiwān lǚxíng, tànsuǒ dāndì de lìshǐ wénhuà hé měishí。\n' +
+			'[00:33.98] 我很期待和家人一起度过一个难忘的假期。Wǒ hěn qīdài hé jiārén yīqǐ dùguò yīgè nánwàng de jiàqī。\n';
+	} else if (traditionalToSimplified) {
+		systemInstructions += '[00:26.92] 明年我计划去中国和台湾旅行，探索当地的历史文化和美食。\n' +
+			'[00:33.98] 我很期待和家人一起度过一个难忘的假期。\n';
+	} else if (addPinyin) {
+		systemInstructions += '[00:26.92] 明年我計劃去中國和台灣旅行，探索當地的歷史文化和美食。míngnián wǒ jìhuà qù zhōngguó hé táiwān lǚxíng, tànsuǒ dāndì de lìshǐ wénhuà hé měishí。\n' +
+			'[00:33.98] 我很期待和家人一起度過一個難忘的假期。Wǒ hěn qīdài hé jiārén yīqǐ dùguò yīgè nánwàng de jiàqī';
+	}
+	systemInstructions += '```\n';
+
+	const completion = await openai.chat.completions.create({
+		model: 'gpt-4o',
+		messages: [
+			{ role: 'system', content: systemInstructions },
+			{
+				role: 'user',
+				content: lyrics
+			}
+		],
+		max_tokens: 4096,
+	});
+	console.log({ completion });
+	const processedLyrics = completion.choices[0].message.content ?? lyrics;
+
+	console.log({ processedLyrics });
 
 	return {
 		albumName: lrcLibLyrics.albumName,
@@ -56,8 +70,7 @@ export async function processLyrics(lrcLibLyrics: LrcLibGetLyricsResponse, openA
 		instrumental: false,
 		plainLyrics: synced ? stripTimestamps(processedLyrics) : processedLyrics,
 		syncedLyrics: synced ? processedLyrics : null,
-		trackName: lrcLibLyrics.trackName,
-
+		trackName: lrcLibLyrics.trackName
 	};
 }
 
